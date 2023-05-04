dbd091b7578837a29110320bb23c9be67c4105cb
FaruqueParvej <omarfaruque1750@gmail.com>
2023-05-04 15:58:21 +0600

    added recipes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.port||5000
+const cors = require('cors')
 
 const chefs = require("./data/chefs.json")
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Server API is running')
@@ -10,6 +13,12 @@ app.get('/', (req, res) => {
 
 app.get("/chefs",(req,res)=>{
     res.send(chefs)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedChef = chefs.find(n => n.id === id);
+    res.send(selectedChef)
 })
 
 app.listen(port, () => {
